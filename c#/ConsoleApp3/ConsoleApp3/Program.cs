0e347165714f64ec00ae991c312603e9929ca37b
karoltomaszewski <karoltomaszewskiit@gmail.com>
2024-05-31 20:37:50 +0200

    Moq tests

---
+++ c#/ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,45 @@
+﻿using Moq;
+using NUnit.Framework;
+
+public interface IWebService
+{
+    void SendData(string data);
+}
+
+public class Calculator
+{
+    private IWebService _webService;
+
+    public Calculator(IWebService webService)
+    {
+        _webService = webService;
+    }
+
+    public int Add(int a, int b)
+    {
+        int result = a + b;
+        _webService.SendData($"Add operation: {a} + {b} = {result}");
+        return result;
+    }
+}
+
+[TestFixture]
+public class CalculatorTests
+{
+    [Test]
+    public void Add_ShouldSendDataToWebService()
+    {
+        // Arrange
+        Mock<IWebService> webServiceMock = new Mock<IWebService>();
+        Calculator calculator = new Calculator(webServiceMock.Object);
+
+        // Act
+        int result = calculator.Add(3, 5);
+
+        // Assert
+        Assert.AreEqual(8, result);
+
+        // Verify that the SendData method was called with the expected argument
+        webServiceMock.Verify(ws => ws.SendData("Add operation: 3 + 5 = 8"), Times.Once);
+    }
+}
